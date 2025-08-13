a265f1ac54d373d0a493f94d7e830cdca9865eb5
OleksandrPinchuk <116524115+OleksandrPinchuk@users.noreply.github.com>
2025-08-13 23:47:41 +0200

    home section desctop screen

--- src/js/faq-question.js
+++ src/js/faq-question.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.faq-question').forEach(btn => {
+document.querySelectorAll('.faq__question').forEach(btn => {
   btn.addEventListener('click', () => {
     const item = btn.parentElement;
     item.classList.toggle('active');
