34ff5e41db43fca44007f1421fcf4027b14cc0d3
OleksandrPinchuk <116524115+OleksandrPinchuk@users.noreply.github.com>
2025-08-24 22:41:14 +0200

    header style

--- src/js/header-scroll.js
+++ src/js/header-scroll.js
@@ -1,9 +1,12 @@
 window.addEventListener("scroll", () => {
   const header = document.getElementById("header");
+  const button = document.getElementById("header__button");
 
   if (window.scrollY > 50) {
     header.classList.add("scrolled");
+    button.classList.add("scrolled-btn");
   } else {
     header.classList.remove("scrolled");
+    button.classList.remove("scrolled-btn");
   }
 });
